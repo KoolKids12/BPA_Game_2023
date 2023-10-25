6a4d250fa9bd5ec2e9b1515cb194f4d5ed589752
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-10-25 13:03:29 -0400

    Update CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -11,6 +11,68 @@ public class CameraMovement : MonoBehaviour
    
     float zoom;
     Vector2 moveDirection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMovement : MonoBehaviour
+{ 
+    [SerializeField] Camera cam;
+    [SerializeField] Camera ortho;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+   
+    float zoom = 10;
+    Vector2 moveDirection;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+       
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        
+        if((ortho.orthographicSize -Input.GetAxis("Mouse ScrollWheel") * zoom) <= 10)
+        {
+
+        }
+        else
+        {
+            ortho.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom;
+
+        }
+
+        if((cam.fieldOfView - Input.GetAxis("Mouse ScrollWheel") * zoom) <= 10)
+        {
+
+        }
+        else
+        {
+            cam.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoom;
+        }
+
+
+    }
+
+     private void FixedUpdate()
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        
+            
+        }
+
+     
+      
+
+}
+
 
 
 
