f687ed6f72d7e2dd29bdc621fc3175431318366d
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-10-27 13:13:45 -0400

    Update to game code

--- DragObjects.cs
+++ DragObjects.cs
@@ -5,16 +5,67 @@ using UnityEngine;
 public class DragObjects : MonoBehaviour
 {
   private bool dragging = false;
+  private bool open = false;
   private Vector3 offset;
+  private Vector3 placement;
+
+  [SerializeField] Camera cam;
+
+  void Start() {
+    cam.enabled = true;
+  }
 
   void Update() {
-    if (dragging) {
-      transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+
+    if(Input.GetMouseButtonDown(1))
+    {
+      if(open == false)
+      {
+        open = true;
+        Debug.Log("Moving Enabled");
+      }
+      else
+      {
+        open = false;
+      }
+    }
+
+    placement.x = transform.position.x;
+    placement.y = transform.position.y;
+
+    if (dragging && transform.position.y <= 40 && open == true) {
+      transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+    }
+    else if(dragging && transform.position.y >= 40)
+    {
+      placement.y = 39;
+
+      transform.position = placement;
+    }
+    else
+    {
+
+    }
+
+    if (dragging && transform.position.x <= 95 && open == true)
+    {
+      transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+
+    }
+    else if(dragging && transform.position.x >=95)
+    {
+      placement.x = 94;
+
+      transform.position = placement;
+
+    }
+    else{
+
     }
   }
 
   private void OnMouseDown() {
-    offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    offset = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
     dragging = true;
   }
 
