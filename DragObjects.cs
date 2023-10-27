26b7c84485b3eb62ca369ad3212073b316469618
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-10-27 10:29:47 -0400

    Update DragObjects.cs

--- DragObjects.cs
+++ DragObjects.cs
@@ -6,15 +6,52 @@ public class DragObjects : MonoBehaviour
 {
   private bool dragging = false;
   private Vector3 offset;
+  private Vector3 placement;
+
+  [SerializeField] Camera cam;
+
+  void Start() {
+    cam.enabled = true;
+  }
 
   void Update() {
-    if (dragging) {
-      transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+
+    placement.x = transform.position.x;
+    placement.y = transform.position.y;
+
+    if (dragging && transform.position.y <= 40 ) {
+      transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+    }
+    else if(dragging && transform.position.y >= 40)
+    {
+      placement.y = 39;
+
+      transform.position = placement;
+    }
+    else
+    {
+
+    }
+
+    if (dragging && transform.position.x <= 95)
+    {
+      transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+
+    }
+    else if(dragging && transform.position.x >=95)
+    {
+      placement.x = 94;
+
+      transform.position = placement;
+
+    }
+    else{
+
     }
   }
 
   private void OnMouseDown() {
-    offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    offset = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
     dragging = true;
   }
 
