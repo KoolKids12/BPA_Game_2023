4795f8770c55516712c660cf320d70c3da93d030
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-10-27 10:29:07 -0400

    Update CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -5,21 +5,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 { 
-    [SerializeField] Camera cam;
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-   
-    float zoom;
-    Vector2 moveDirection;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public class CameraMovement : MonoBehaviour
-{ 
-    [SerializeField] Camera cam;
-    [SerializeField] Camera ortho;
+    [SerializeField] Camera Main;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
    
@@ -37,55 +23,18 @@ public class CameraMovement : MonoBehaviour
 
        
         moveDirection = new Vector2(moveX, moveY).normalized;
+
         
-        if((ortho.orthographicSize -Input.GetAxis("Mouse ScrollWheel") * zoom) <= 10)
+        if((Main.orthographicSize -Input.GetAxis("Mouse ScrollWheel") * zoom) <= 10 || (Main.orthographicSize -Input.GetAxis("Mouse ScrollWheel") * zoom) >= 45 )
         {
 
         }
         else
         {
-            ortho.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom;
+            Main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom;
 
         }
 
-        if((cam.fieldOfView - Input.GetAxis("Mouse ScrollWheel") * zoom) <= 10)
-        {
-
-        }
-        else
-        {
-            cam.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoom;
-        }
-
-
-    }
-
-     private void FixedUpdate()
-        {
-            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-
-        
-            
-        }
-
-     
-      
-
-}
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        cam.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * zoom;
-        
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-       
-        moveDirection = new Vector2(moveX, moveY).normalized;
     }
 
      private void FixedUpdate()
