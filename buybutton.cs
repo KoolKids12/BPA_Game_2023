009dcde956d6cc2aa7970f8c1194327fe8b21c4b
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-11-16 21:30:50 -0500

    Add files via upload
    It went good for a while

--- buybutton.cs
+++ buybutton.cs
@@ -4,31 +4,86 @@ using UnityEngine;
 
 public class buybutton : MonoBehaviour
 {
+    public GameObject = SpawnGrunt;
+    public GameObject = SpawnHogRider;
+    public GameObject = SpawnArmVikingGuy;
+
     public GameObject SquarePrefab;
-    private bool spawningEnabled = false;
-    private int clicksRemaining = 0;
+    [SerializeField] public bool spawningEnabled1 = false;
+    [SerializeField] public bool spawningEnabled2 = false;
+    [SerializeField] public bool spawningEnabled3 = false;
+
+    [SerializeField] int clicksRemaining1 = 0;
+    [SerializeField] int clicksRemaining2 = 0;
+    [SerializeField] int clicksRemaining3 = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (spawningEnabled && clicksRemaining > 0 && Input.GetMouseButtonDown(0))
+        if (spawningEnabled1 && clicksRemaining1 > 0 && Input.GetMouseButtonDown(0))
         {
             SpawnBuilding();
-            clicksRemaining--;
+            clicksRemaining1--;
 
-            if (clicksRemaining == 0)
+            if (clicksRemaining1 == 0)
             {
                 Debug.Log("No more clicks remaining");
-                spawningEnabled = false;
+                spawningEnabled1 = false;
+            }
+        }
+        else if (spawningEnabled2 && clicksRemaining2 > 0 && Input.GetMouseButtonDown(0))
+        {
+            SpawnBuilding();
+            clicksRemaining2--;
+
+            if (clicksRemaining2 == 0)
+            {
+                Debug.Log("No more clicks remaining");
+                spawningEnabled2 = false;
+            }
+        }
+        else if (spawningEnabled3 && clicksRemaining3 > 0 && Input.GetMouseButtonDown(0))
+        {
+            SpawnBuilding();
+            clicksRemaining3--;
+
+            if (clicksRemaining3 == 0)
+            {
+                Debug.Log("No more clicks remaining");
+                spawningEnabled3 = false;
             }
         }
     }
 
-    public void BuyButtonClicked()
+    public void ButtonGrunt()
     {
-        spawningEnabled = true;
-        clicksRemaining = 10;
-        Debug.Log("Buy button clicked! You have 10 clicks to spawn the block.");
+        if (gameObject.tag == "GruntSpawn")
+        {
+            spawningEnabled1 = true;
+            spawningEnabled2 = false;
+            spawningEnabled3 = false;
+        }
+    }
+
+    public void ButtonHogRider()
+    {
+        if (gameObject.tag == "HogRiderSpawn")
+        {
+            spawningEnabled1 = false;
+            spawningEnabled2 = true;
+            spawningEnabled3 = false;
+        }
+    }
+
+    public void ButtonArmVikingGuy()
+    {
+        if (gameObject.tag == "ArmVikingGuySpawn")
+        {
+            spawningEnabled1 = false;
+            spawningEnabled2 = false;
+            spawningEnabled3 = true;
+        }
     }
 
     void SpawnBuilding()
