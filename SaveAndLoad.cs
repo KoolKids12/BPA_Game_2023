a787a4fd978349e9bf853b62666af4ae224a1f91
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-12-01 13:36:00 -0500

    Add files via upload

--- SaveAndLoad.cs
+++ SaveAndLoad.cs
@@ -7,15 +7,15 @@ public class SaveAndLoad : MonoBehaviour
     // Start is called before the first frame updatepublic class SaveAndLoad : MonoBehaviour
 
     
-    public string json = "";
-     public int i;
+    private string json = "";
+     private int i;
+    [SerializeField] public GameObject thingprefab, thingprefab2, thingprefab3;
 
 
     void Awake() 
     {
         SaveSystem.Init();
-        FakeSave();
-        //LoadGame();
+        LoadGame();
         Debug.Log("Done");
     }
     public void SaveGame()
@@ -26,12 +26,12 @@ public class SaveAndLoad : MonoBehaviour
         {
             SaveObject saveObject = new SaveObject
             {
-                locate = allObjects[i].transform.position
+                locate = allObjects[i].transform.position,
 
-                
+                tagg = allObjects[i].tag,
+
+                names = allObjects[i].name
             };
-            
-            allObjects[i].name = i.ToString();
             
             json = JsonUtility.ToJson(saveObject);
 
@@ -49,55 +49,37 @@ public class SaveAndLoad : MonoBehaviour
 
     }
 
-       
-    
-    public void FakeSave()
-    {
-        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-
-        for  ( i = 1; i<= allObjects.Length; i++)
-        {
-            SaveObject saveObject = new SaveObject
-            {
-                locate = allObjects[i].transform.position
-
-                
-            };
-            
-            allObjects[i].name = i.ToString();
-            
-            json = JsonUtility.ToJson(saveObject);
-            
-        }
-        
-        
-
-
-
-    }
     public void LoadGame()
     {
-
-        GameObject gameObject;
-        
+        GameObject[] remover;
 
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
 
         for  ( i = 1; i<= allObjects.Length; i++)
         {
-            allObjects[i].name = i.ToString();
-
             json = SaveSystem.Load(i);
         
             SaveObject loadedObject = JsonUtility.FromJson<SaveObject>(json);
             
-            gameObject = allObjects[i];
+            remover = GameObject.FindGameObjectsWithTag(loadedObject.tagg);
 
-               Debug.Log(gameObject.name);
-          
+           //Debug.Log(remover[i]);
 
-                gameObject.transform.position = loadedObject.locate;
+            Destroy(remover[i]);
             
+            if(loadedObject.tagg == "RefinedHolium1")
+            {
+                Instantiate(thingprefab, loadedObject.locate, Quaternion.identity);
+            }
+            if(loadedObject.tagg == "RefinedHolium2")
+            {
+                Instantiate(thingprefab2, loadedObject.locate, Quaternion.identity);
+            }
+            if(loadedObject.tagg == "RefinedHolium3")
+            {
+            Instantiate(thingprefab3, loadedObject.locate, Quaternion.identity);
+            }
+
             
         }
                
@@ -112,4 +94,6 @@ public class SaveAndLoad : MonoBehaviour
 class SaveObject
 {
     public Vector3 locate;   
+    public string tagg;
+    public string names;
 }
